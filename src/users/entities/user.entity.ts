ad788d60f0a888ef8e10579ad87be2885fd6ec35
pkarasev <p.karasev@psw.ru>
2024-05-20 16:01:30 +0300

    upd

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,36 @@
-export class User {}
+import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+
+@Entity()
+export class User {
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: "varchar",
+    length: 30,
+    unique: true,
+  })
+  username: string;
+
+  @Column()
+  about: string;
+
+  @Column()
+  avatar: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+//   @Column()
+//   wishes: string;
+  
+//   @Column()
+//   offers: string;
+  
+//   @Column()
+//   wishlists: string;
+}
