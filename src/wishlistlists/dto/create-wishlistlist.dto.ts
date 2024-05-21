e77b23a444ad772e018a1a9f8ab3668f4ea080e5
pkarasev <p.karasev@psw.ru>
2024-05-21 14:21:14 +0300

    upd

--- src/wishlistlists/dto/create-wishlistlist.dto.ts
+++ src/wishlistlists/dto/create-wishlistlist.dto.ts
@@ -1,1 +1,18 @@
-export class CreateWishlistlistDto {}
+import { IsString, IsOptional, IsUrl, IsArray, Length, Max } from 'class-validator';
+
+export class CreateWishlistlistDto {
+    @IsString()
+    @Length(1, 250)
+    name: string;
+
+    @IsUrl()
+    image: string; // "https://i.pravatar.cc/150?img=3",
+
+    @IsOptional()
+    @IsString()
+    @Length(0, 1500)
+    description?: string;
+
+    @IsArray()
+    itemsId: number[]
+}
--- src/wishlistlists/wishlistlists.controller.ts
+++ src/wishlistlists/wishlistlists.controller.ts
@@ -5,30 +5,35 @@ import { UpdateWishlistlistDto } from './dto/update-wishlistlist.dto';
 
 @Controller('wishlistlists')
 export class WishlistlistsController {
-  constructor(private readonly wishlistlistsService: WishlistlistsService) {}
+  constructor(private readonly wishlistlistsService: WishlistlistsService) { }
 
   @Post()
   create(@Body() createWishlistlistDto: CreateWishlistlistDto) {
-    return this.wishlistlistsService.create(createWishlistlistDto);
+    return ({ post: createWishlistlistDto });
+    // return this.wishlistlistsService.create(createWishlistlistDto);
   }
 
   @Get()
-  findAll() {
-    return this.wishlistlistsService.findAll();
+  getAll() {
+    return ({ get: `/wishlistlists` });
+    // return this.wishlistlistsService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.wishlistlistsService.findOne(+id);
+  getId(@Param('id') id: number) {
+    return ({ get: `/wishlistlists/${id}` });
+    // return this.wishlistlistsService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateWishlistlistDto: UpdateWishlistlistDto) {
-    return this.wishlistlistsService.update(+id, updateWishlistlistDto);
+  patchId(@Param('id') id: number, @Body() updateWishlistlistDto: UpdateWishlistlistDto) {
+    return ({ patch: updateWishlistlistDto });
+    // return this.wishlistlistsService.update(+id, updateWishlistlistDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.wishlistlistsService.remove(+id);
+  deleteId(@Param('id') id: number) {
+    return ({ delete: `/wishlistlists/${id}` });
+    // return this.wishlistlistsService.remove(+id);
   }
 }
